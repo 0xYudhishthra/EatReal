70fa51971bc2967fb59ffdfc06e8ab178262c7d1
Cheong Yee Chian <cheongyeechian@gmail.com>
2023-12-18 13:06:08 +0800

    Remove card components

--- src/app/components/CardOne.tsx
+++ src/app/components/CardOne.tsx
@@ -1,6 +1,6 @@
 "use client"
 import React, { useState } from 'react';
-import './App.css'; // Make sure to create an App.css file for styling
+import Cards from '../components/Cards';
 
 // Define TypeScript interfaces for the card properties
 interface CardProps {
@@ -9,19 +9,11 @@ interface CardProps {
   title: string;
   poweredBy: string;
   onNavigate: () => void;
+
 }
 
 // Create the card component
-const Card: React.FC<CardProps> = ({ year, eventsCount, title, poweredBy, onNavigate}) => {
-  return (
-    <div className="card" onClick={onNavigate}>
-      <div className="card-year">{year}</div>
-      <div className="card-events">{eventsCount} Events</div>
-      <div className="card-title">{title}</div>
-      <div className="card-powered">{poweredBy}</div>
-    </div>
-  );
-};
+
 
 // Main App component where you use the card component
 const CardOne: React.FC<{ onNavigate: (page: 'MainPage' | 'CardOne' | 'CardTwo') => void }> = ({ onNavigate }) => {
@@ -32,8 +24,14 @@ const CardOne: React.FC<{ onNavigate: (page: 'MainPage' | 'CardOne' | 'CardTwo')
         <p>Click here to get see what's AFI is in your NFT</p>
       </header>
       <div className="cards-container">
-        <Card year={2023} eventsCount={32} title="The 'Degen'" poweredBy="powered by ERC-6551" onNavigate={() => onNavigate("CardOne")}/>
-        
+      <Cards 
+          year={2023} 
+          eventsCount={32} 
+          title="The 'Degen'" 
+          poweredBy="powered by ERC-6551" 
+          onNavigate={() => onNavigate("CardOne")}
+          image="/hello.jpg" // Update with the path to your first image
+        />
       </div>
     </div>
   );
--- src/app/components/CardTwo.tsx
+++ src/app/components/CardTwo.tsx
@@ -1,6 +1,6 @@
 "use client"
 import React, { useState } from 'react';
-import './App.css'; // Make sure to create an App.css file for styling
+import Cards from '../components/Cards';
 
 // Define TypeScript interfaces for the card properties
 interface CardProps {
@@ -9,19 +9,9 @@ interface CardProps {
   title: string;
   poweredBy: string;
   onNavigate: () => void;
+
 }
 
-// Create the card component
-const Card: React.FC<CardProps> = ({ year, eventsCount, title, poweredBy, onNavigate}) => {
-  return (
-    <div className="card" onClick={onNavigate}>
-      <div className="card-year">{year}</div>
-      <div className="card-events">{eventsCount} Events</div>
-      <div className="card-title">{title}</div>
-      <div className="card-powered">{poweredBy}</div>
-    </div>
-  );
-};
 
 // Main App component where you use the card component
 const CardTwo: React.FC<{ onNavigate: (page: 'MainPage' | 'CardOne' | 'CardTwo') => void }> = ({ onNavigate }) => {
@@ -32,8 +22,14 @@ const CardTwo: React.FC<{ onNavigate: (page: 'MainPage' | 'CardOne' | 'CardTwo')
         <p>Click here to get see what's AFI is in your NFT</p>
       </header>
       <div className="cards-container">
-        <Card year={2024} eventsCount={32} title="The 'Degen'" poweredBy="powered by ERC-6551" onNavigate={() => onNavigate("MainPage")}/>
-        
+      <Cards 
+          year={2024} 
+          eventsCount={32} 
+          title="The 'Degen'" 
+          poweredBy="powered by ERC-6551" 
+          onNavigate={() => onNavigate("CardTwo")}
+          image="/Untitled.png" // Update with the path to your second image
+        />
       </div>
     </div>
   );
