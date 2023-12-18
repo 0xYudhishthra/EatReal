f4540d85efaa8aef1909b8dddae03ce017aa3dfe
Cheong Yee Chian <cheongyeechian@gmail.com>
2023-12-18 12:08:35 +0800

    import AA

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import EventCard from './components/EventCard'; 
-
+import AA from './components/AA';
 
   export default function Home() {
     return (
       <div className="flex min-h-screen flex-col ">
           <main>
-          <EventCard />
+           <AA />
           </main>
           
       </div>
