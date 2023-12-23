a7e06bd009b256967eb5b9104aa7ed5e544423e8
Liew Chern Jin <69305479+codyy6@users.noreply.github.com>
2023-12-23 20:00:10 +0800

    ethsingaporecard

--- src/app/components/CardOne-2023/ETHSingaporeCard.tsx
+++ src/app/components/CardOne-2023/ETHSingaporeCard.tsx
@@ -5,9 +5,10 @@ import PersonCard from '../PersonCard';
 
 const ETHSingaporeCard: React.FC<{ onNavigate: (page: 'MainPage' | 'CardOne' | 'CardTwo' | 'ETHSingaporeCard' ) => void }> = ({ onNavigate }) => {
     return (
-        <div >
+      <div className='flex'>
+        <div>
           <div>
-            <button></button>
+            <button>Back</button>
           </div>
           <div className="flex items-center justify-center">
             <Cards 
@@ -33,17 +34,39 @@ const ETHSingaporeCard: React.FC<{ onNavigate: (page: 'MainPage' | 'CardOne' | '
               image="/hello.jpg" // Update with the path to your first image
             />
           </div>
-          <div className="flex items-center justify-center h-screen">
-  <div className="relative">
-    <div className="w-24 h-24 bg-gray-300 rounded-full flex items-center justify-center">
-      <div className="w-16 h-16 bg-blue-500 rounded-full flex items-center justify-center">
-        <span className="text-white">Center</span>
-      </div>
-    </div>
-  </div>
-</div>
+          <div className="flex items-center justify-center mt-20">
+          <div className="relative">
+            <div className="w-[300px] h-[200px] bg-gray-300 rounded-[50%] flex items-center justify-center">
+              <button className="w-[225px] h-[150px] bg-blue-500 rounded-[50%] flex items-center justify-center">
+                <span className="text-white justify-center">Connections Made</span>
+              </button>
+            </div>
+          </div>
         </div>
-    
+        <div className='flex justify-between mt-28 mx-40'>
+          <PersonCard 
+              name={"Cody"} 
+              score={0} 
+              title="ETH Singapore" 
+              image="/hello.jpg" // Update with the path to your first image
+            />
+            <PersonCard 
+              name={"Cody"} 
+              score={0} 
+              title="ETH Singapore" 
+              image="/hello.jpg" // Update with the path to your first image
+            />
+          </div>
+          <div className='mt-20 flex justify-center font-semibold text-2xl'>
+            Create Your Memories
+          </div>
+{/* end of the div */}
+        </div>
+        <div className='w-auto'>
+          test
+        </div>
+      </div>
+        
     );
   };
   
