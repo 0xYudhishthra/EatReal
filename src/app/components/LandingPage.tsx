1c553688729130aef8855eb777a3e5e91fad583b
Liew Chern Jin <69305479+codyy6@users.noreply.github.com>
2023-12-25 01:38:37 +0800

    Year get based on years registered

--- src/app/components/LandingPage.tsx
+++ src/app/components/LandingPage.tsx
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import {
   ConnectWallet,
+  contractType,
   useAddress,
   useContract,
   useContractRead,
@@ -10,6 +11,7 @@ import {
 import Cards from "../components/Cards";
 import { useNomad3 } from "./ContractInteractions";
 import { ethers } from "ethers";
+import { BigNumber } from 'ethers';
 
 const LandingPage: React.FC<{
   onNavigate: (
@@ -22,19 +24,48 @@ const LandingPage: React.FC<{
   ) => void;
 }> = ({ onNavigate }) => {
   const address = useAddress();
-
-  const {
-    contract: Nomad3,
-    isLoading: isNomad3Loading,
-    error: nomad3Error,
-  } = useNomad3();
+  const [inputValue, setInputValue] = useState<number | string>("");
+  const [yearsData, setYearsData] = useState<BigNumber[]>([]);
+  const { contract: Nomad3 } = useNomad3();
+  
 
   const { data, isLoading, error } = useContractRead(
     Nomad3,
-    "eventMinterContractAddress"
+    "getYears",
+    [],
+    {
+      from: address
+    }
   );
 
-  console.log(data);
+  const eventsCountData = (year: number) => {
+    return useContractRead(
+      Nomad3,
+      "getEventCount",
+      [year.toString()],  // Use the provided year as an argument
+      {
+        from: address
+      }
+    );
+  };
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        setYearsData(data);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+
+    fetchData();
+  }, [Nomad3, address, data]);
+
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const value = event.target.value;
+    const intValue = /^\d+$/.test(value) ? parseInt(value, 10) : "";
+    setInputValue(intValue);
+  };
 
   const {
     mutateAsync,
@@ -56,39 +87,41 @@ const LandingPage: React.FC<{
             </div>
           </div>
           <div className="grid grid-cols-1 md:grid-cols-12 gap-4 p-4">
-            <div className="md:col-span-4 col-start-3">
-              <Cards
-                year={2023}
-                eventsCount={32}
-                title="The 'Degen'"
-                poweredBy="powered by ERC-6551"
-                onNavigate={() => onNavigate("CardOne")}
-                image="/hello.jpg"
-              />
-            </div>
-
-            <div className="md:col-span-4 col-start-8">
-              <Cards
-                year={2024}
-                eventsCount={32}
-                title="The 'Degen'"
-                poweredBy="powered by ERC-6551"
-                onNavigate={() => onNavigate("CardTwo")}
-                image="/3.jpeg"
-              />
-            </div>
+            {yearsData && yearsData.map((year, index) => (
+              <div key={index} className="md:col-span-4 col-start-3 p-8">
+                <Cards
+                  year={parseInt(year._hex, 16).toString()}   // Access the value from the _hex property
+                  eventsCount={32}
+                  title={`The 'Degen' - Year ${parseInt(year._hex, 16).toString()}`} // Convert here as well                  
+                  poweredBy="powered by ERC-6551"
+                  onNavigate={() => onNavigate("CardOne")}
+                  image={`1.jpeg`}// to fetch later using react hook
+                />
+              </div>
+            ))}
           </div>
-          <Web3Button
-            contractAddress={Nomad3?.getAddress() || ""}
-            contractAbi={Nomad3?.abi}
-            // Calls the "setName" function on your smart contract with "My Name" as the first argument
-            action={() => mutateAsync({ args: [2019] })}
-            onSubmit={() => console.log("Transaction submitted")}
-            onSuccess={(result) => console.log(result)}
-            onError={(error) => console.log(error)}
-          >
-            Send Transaction
-          </Web3Button>
+          <div className="p-5">
+            <input
+              type="text"
+              pattern="\d*"
+              value={inputValue}
+              onChange={handleInputChange}
+              placeholder="Enter Year"
+              className="text-black mr-8" // Added mb-3 for bottom margin
+            />
+
+            <Web3Button
+              contractAddress={Nomad3?.getAddress() || ""}
+              contractAbi={Nomad3?.abi}
+              action={() => mutateAsync({ args: [ethers.BigNumber.from(inputValue)] })}
+              onSubmit={() => console.log("Transaction submitted")}
+              onSuccess={(result) => console.log(result)}
+              onError={(error) => console.log(error)}
+              className="ml-8" // Added mb-3 for bottom margin
+            >
+              Send Transaction
+            </Web3Button>
+          </div>
         </div>
       ) : (
         <div className="flex flex-col items-center justify-center flex-1">
