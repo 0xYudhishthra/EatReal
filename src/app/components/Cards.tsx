6124ad04d072698375c26f6409c4d83e8788fb2c
Cheong Yee Chian <cheongyeechian@gmail.com>
2024-01-08 00:14:07 +0000

    edit

--- src/app/components/Cards.tsx
+++ src/app/components/Cards.tsx
@@ -1,4 +1,3 @@
-import { title } from "process";
 import React from "react";
 
 interface CardProps {
@@ -19,27 +18,21 @@ const Cards: React.FC<CardProps> = ({
   image,
 }) => {
   return (
-    <div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700" onClick={onNavigate}>
-    <div>
+    <div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 container" onClick={onNavigate}>
+      <div>
         <img className="rounded-t-lg" src={image} alt={title} />
-    </div>
-    <div className="p-5">
+      </div>
+      <div className="p-5">
         <div>
-            <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{title}</h5>
+          <h5 className="mb-2 text-xl font-bold tracking-tight text-gray-900 dark:text-white overflow-hidden text-ellipsis whitespace-nowrap">{title}</h5>
         </div>
-        <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">{eventsCount}{year}</p>
+        <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">{eventsCount} events - {year}</p>
         <div className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-            {poweredBy}
+          {poweredBy}
         </div>
+      </div>
     </div>
-</div>
   );
 };
-
-
-
-
-
-
 
 export default Cards;
